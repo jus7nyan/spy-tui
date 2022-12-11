aeaa623d25b612191f14ee784100f3f4b74c0f97
ただにゃん <87938588+jus7nyan@users.noreply.github.com>
2022-12-11 21:29:38 +0400

    Update spytui.py

--- spytui.py
+++ spytui.py
@@ -1,44 +1,142 @@
 import os
+import keyboard as kb
 
 class Window:
     def __init__(self):
         rows, columns = os.popen('stty size', 'r').read().split()
         self.Rows, self.Columns = int(rows),int(columns)
-        self.Grid = (3,3)
+        self.Name = "Window"
+        self.grid = []
         self.Objects = []
-        
-    def update(self):
+        self.hotkeys = []
+    
+    def update_size(self):
         rows, columns = os.popen('stty size', 'r').read().split()
         self.Rows, self.Columns = int(rows),int(columns)
+        return rows, columns
+
+    def set_grid(self,n):
+        for i in range(n):
+            self.grid.append([])
+
+    def addObject(self, *args):
+        for i in args:
+            self.grid[i.gridpos].append(i)
+            self.Objects.append(i)
+    
+    def dellObject(self, *args):
+        for i in args:
+            self.grid[i.gridpos]
+            self.Objects.remove(i)
     
     def render(self):
-        self.update()
-        grid = []
-
-        
+        rows,columns = int(self.update_size()[0]), int(self.update_size()[1])
         screen = ""
-        for i in range(self.Grid[0]):
-            grid.append([])
-        for i in self.Objects:
-            for j in i.result():
-                screen += str(j)+""
-        return screen
+        
+        objects = self.Objects
+        
+        for i in objects:
+            result = i.result()
+            pos = i.position
+            if pos == "center":
+                for j in result:
+                    if i.View == "simple":
+                        screen += " "*(columns // 2 - (len(i.Text)//2+2)) + j + "\n"
+                    else:
+                        screen += " "*(columns // 2 - (len(result[0])//2+2)) + j + "\n"
+
+            elif pos == "left":
+                for j in result:
+                    screen += j + "\n"
+            elif pos == "right":
+                for j in result:
+                    screen += " "*(columns - len(result[0]))+j+"\n"
+            else:
+                return "Position ERROR"
+        return rows, columns, screen
     
-    def print(self, screen):
+    def gridrender(self):
+        rows,columns = int(self.update_size()[0]), int(self.update_size()[1])
+        screen = ""
+        
+        objects = self.grid
+        
+        for i in range(len(objects)):
+            for j in range(len(objects[i])):
+                pass
+                
+    
+    def scprint(self):
+        rows, columns, screen = self.render()
+        screen = " "*(columns//2-len(self.Name))+self.Name+"\n"+"═"*columns+screen
+
+        os.system("clear")
         print(screen)
     
-    def addObject(self, object):
-        try:
-            self.Objects.append(object)
-            return 0
-        except:
-            return 1
+    def Change_selection(self,arg):
+        obj = self.Objects
+        if arg == "next":
+            for i in range(len(obj)):
+                if obj[i].is_choise == True:
+                    try:
+                        if obj[i+1].can_choise == True:
+                            obj[i+1].is_choise = True
+                        else:
+                            continue
+                    except:
+                        if obj[(len(obj)-1)-i].can_choise == True:
+                            obj[(len(obj)-1)-i].is_choise = True
+                        else:
+                            continue
+                    break
+            obj[i].is_choise = False
+        elif arg == "perv":
+            for i in range(len(obj)):
+                if obj[i].is_choise == True:
+                    try:
+                        if obj[i-1].can_choise == True:
+                           obj[i-1].is_choise = True
+                        else:
+                            continue
+                    except:
+                        if obj[i-len(obj)].can_choise == True:
+                            obj[i-len(obj)].is_choise = True
+                        else:
+                            continue
+                    break
+            obj[i].is_choise = False
 
-    def dellObject(self, object):
-        try:
-            for i in self.Objects:
-                if i == object:
-                    self.Objects.remove(object)
-            return 0
-        except:
-            return 1
+    
+    def switch_press(self):
+        ob = self.Objects
+        for i in obj:
+            pass
+    
+    def Manipulate(self):
+        self.scprint()
+        event = kb.read_event()
+        obj = self.Objects
+
+        if event.event_type == kb.KEY_DOWN:
+            if event.name == "up":
+                self.Change_selection("perv")
+                self.scprint()
+            
+            elif event.name == "down":
+                self.Change_selection("next")
+                self.scprint()
+                
+            elif event.name == "space":
+                for i in obj:
+                    if i.is_choise == True and i.can_press == True:
+                        func = i.Func
+                        func(self)
+                self.scprint()
+
+            else:
+                self.scprint()
+        return self
+
+
+    def setName(self,Name):
+        self.Name = Name
