44bda2254a119f08ea9aa1b48ae25eeec141fab3
ただにゃん <87938588+jus7nyan@users.noreply.github.com>
2022-12-11 21:29:06 +0400

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,151 +1,166 @@
-from colorama import init
+from colorama import init, Fore, Back, Style
 init()
-from colorama import Fore, Back, Style
-
-
 
 class View:
-    
     def __init__(self):
-        
-        self.simple = ["","\n"]
-        
-        self.single =  ["┌","", "┐\n",
-                        "│","", "│\n",
-                        "└","", "┘\n"]
-        
-        self.double =  ["╔","" ,"╗\n",
-                        "║","", "║\n",
-                        "╚","" ,"╝\n"]
+        self.single = ["┌","─","┐",
+                       "│",
+                       "└",    "┘"]
+        self.double = ["╔","═","╗",
+                       "║",
+                       "╚",    "╝"]
         self.choise = Fore.BLACK + Back.WHITE
         self.end = Style.RESET_ALL
     
-    def get(self,name, text, is_choise):
-        if name == "single" and is_choise:
-            view = self.single
-            view[4] = self.choise + text +self.end
-            for i in range(len(text)):
-                view.insert(1,"─")
-            for i in range(len(text)):
-                view.insert(len(text)+7, "─")
-            return view
-
-        elif name == "double" and is_choise:
-            view = self.double
-            view[4] = self.choise + text + self.end
-            for i in range(len(text)):
-                view.insert(1,"═")
-            for i in range(len(text)):
-                view.insert(len(text)+7, "═")
-            return view
+    def get(self, object):
+        view = object.View
+        text = object.Text
+        choise = object.is_choise
+        text = text.split("\n")
+        maxlen = len(max(text))
+        rows = len(text)
         
-        elif name == "simple" and is_choise:
-            view = self.simple
-            view[0] = self.choise + text +self.end
-            return view
+        if view == "simple":
+            result = []
+            if choise == True:
+
+                for i in range(rows):
+                    result.append(self.choise + text[i] +" "*(maxlen-len(text[i]))+ self.end)
+            else:
+                for i in range(rows):
+                    result.append(text[i] +" "*(maxlen-len(text[i])))
+            return result
         
-        elif name == "single":
-            view = self.single
-            view[4] = text
-            for i in range(len(text)):
-                view.insert(1,"─")
-            for i in range(len(text)):
-                view.insert(len(text)+7, "─")
-            return view
+        elif view == "single":
+            if choise == True:
+                
+                result0 = [self.single[0]+self.single[1]*maxlen+self.single[2]]
+                resultt = []
+                result1 = [self.single[4]+self.single[1]*maxlen+self.single[5]]
+                
+                for i in range(rows):
+                    resultt.append(self.single[3]+self.choise+text[i]+" "*(maxlen-len(text[i]))+self.end+self.single[3])
+                
+                result = result0+resultt+result1
+                
+                return result
+            else:
 
-        elif name == "double":
-            view = self.double
-            view[4] = text
-            for i in range(len(text)):
-                view.insert(1,"═")
-            for i in range(len(text)):
-                view.insert(len(text)+7, "═")
-            return view
+                
+                result0 = [self.single[0]+self.single[1]*maxlen+self.single[2]]
+                resultt = []
+                result1 = [self.single[4]+self.single[1]*maxlen+self.single[5]]
+                
+                for i in range(rows):
+                    resultt.append(self.single[3]+text[i]+" "*(maxlen-len(text[i]))+self.single[3])
+                
+                result = result0+resultt+result1
         
-        elif name == "simple":
-            view = self.simple
-            view[0] = text
-            return view
+        elif view == "double":
+            if choise == True:
+                
+                result0 = [self.double[0]+self.double[1]*maxlen+self.double[2]]
+                resultt = []
+                result1 = [self.double[4]+self.double[1]*maxlen+self.double[5]]
+                
+                for i in range(rows):
+                    resultt.append(self.double[3]+self.choise+text[i]+" "*(maxlen-len(text[i]))+self.end+self.double[3])
+                
+                result = result0+resultt+result1
+                
+                return result
+            else:
+                
+                result0 = [self.double[0]+self.double[1]*maxlen+self.double[2]]
+                resultt = []
+                result1 = [self.double[4]+self.double[1]*maxlen+self.double[5]]
+                
+                for i in range(rows):
+                    resultt.append(self.double[3]+text[i]+" "*(maxlen-len(text[i]))+self.double[3])
+                
+                result = result0+resultt+result1
+                
+        return result
 
-
+                                    # Label class
 class Label:
-
     def __init__(self):
-        self.is_choise = False
-        self.Text = ""
-        self.Pos = "center"
-        self.Neighbors = 0
-        self.View = "simple"
+        self.Text = ""                              # text of the label
+        self.View = "simple"                        # what like label look
+        self.is_choise = False                      
+        self.can_choise = False
+        self.can_press = False
+        self.position = [0, "center"]
+        self.len = len(self.Text)
+        self.gridpos = 0
+    
+    def set(self, text = -1,
+            view = -1,
+            is_choise = -1,
+            can_choise = -1,
+            func = -1,
+            pos = -1,
+            gridpos = -1):
         
-    
-    def get_info(self, info):
-        if info == "text":
-            info = self.Text
-        elif info == "pos":
-            info = self.Pos
-        elif info == "view":
-            view = self.View
-        elif info == "result":
-            info = self.result()
-        elif info == "is_choise":
-            info = self.is_choise
-        elif info == "all":
-            info = [self.Text, self.Pos, self.View, self.is_choise, self.result()]
-        return info
-    
-    def set(self, text="", pos="center", neighbors=0, view="simple", is_choise = False):
-        try:
+        
+        if text != -1:
             self.Text = text
-            self.Pos = pos
+        if view != -1:
             self.View = view
+        if is_choise != -1:
             self.is_choise = is_choise
-            self.Neighbors = neighbors
-        except:
-            return 1
+        if can_choise != -1:
+            self.can_choise = can_choise
+        if pos != -1:
+            self.position = pos
+        if gridpos != -1:
+            self.gridpos = gridpos
     
     def result(self):
-        return (View().get(self.View, self.Text, self.is_choise))
+        return View().get(self)
 
 
+
+
+                                            # Button class
 class Button:
-
     def __init__(self):
-        self.is_choise = False
-        self.Text = ""
-        self.Pos = "center"
-        self.Neighbors = 0
-        self.View = "simple"
-        self.is_pressed = False
-        self.Func = "on_pressed"
+        self.Text = ""                          # Text on button
+        self.View = "simple"                    # what like button look
+        self.is_choise = False                  # whether the button is selected
+        self.is_pressed = False                 # whether the button is pressed
+        self.can_choise = True                  # is it possible to choise the button
+        self.can_press = True                   # is it possible to press the button
+        self.Func = self.base_func              # function of the button
+        self.position = "center"                # lowercase button position
+        self.len = len(self.Text)               # button text length
+        self.gridpos = 0                        # button position in a column
     
-    def get_info(self, info):
-        if info == "text":
-            info = self.Text
-        elif info == "pos":
-            info = self.Pos
-        elif info == "view":
-            view = self.View
-        elif info == "result":
-            info = self.result()
-        elif info == "is_choise":
-            info = self.is_choise
-        elif info == "is_pressed":
-            info = self.is_Pressed
-        elif info == "all":
-            info = [self.Text, self.Pos, self.View, self.is_choise, self.is_pressed, self.Neighbors, self.result()]
-        return info
+    def base_func():
+        return 0
     
-    def set(self, text="", pos="center", neighbors=0, view="simple", is_choise = False, is_pressed=False, func = "on_pressed"):
-        try:
+    def set(self, text = -1,                    # set the parameters of button
+            view = -1,
+            is_choise = -1,
+            can_choise = -1,
+            func = -1,
+            pos = -1,
+            gridpos = -1):
+        
+        if text != -1:
             self.Text = text
-            self.Pos = pos
+        if view != -1:
             self.View = view
+        if is_choise != -1:
             self.is_choise = is_choise
-            self.Neighbors = neighbors
-            self.is_pressed = is_pressed
+        if can_choise != -1:
+            self.can_choise = can_choise
+        if pos != -1:
+            self.position = pos
+        if func != -1:
             self.Func = func
-        except:
-            return 1
-    
-    def result(self):
-        return (View().get(self.View, self.Text, self.is_choise, self,is_pressed))
+        if gridpos != -1:
+            self.gridpos = gridpos
+        
+    def result(self):                           # end view of the button
+        return View().get(self)
